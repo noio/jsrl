c2e399733d1ead3398ddf9d8a9a2138498164b9e
Thomas van den Berg <paranoid.ndroid@gmail.com>
2013-06-09 16:01:06 +0200

    indentation

--- js/utils.js
+++ js/utils.js
@@ -17,139 +17,78 @@ function showDataTable(data, title){
     $(myWindow.document.body).append(table);
 }
 
-/**
-* Does what?
-*/
-function barPlot(canvas, n_tries, total_reward, standard_errors) {
-        
-
-        var keys = []
-        var y_values1 = []
-        var y_values2 = []
-        
-
-        for (var key in n_tries) {
-            keys.push(key)
-            y_values1.push(n_tries[key])
-            y_values2.push(total_reward[key] / n_tries[key])
-        }
-        
-        
-        // Create xticks
-        var x_ticks = []
-        var x_value = 0.5
-        for (var i = 0; i < keys.length; i++) {
-            x_ticks.push([x_value, keys[i]])
-            x_value += 1
-        }
-
-        
-        var points = {show: true}
-        
-        // Set up bar plot
-        var options = {
-            bars: {
-                barWidth: 0.25,
-                show: true,
-                align: "center",
-                radius: 5
-
-            },
-            xaxis: {
-                min: 0,
-                max: keys.length,
-                ticks: x_ticks
-            },
-            yaxis: {min: 0},
-            yaxis2: {min: 0, 
-                    max: 1}
-        };
-
-        var data1 = []
-        var data2 = []
-        
-        for (var i = 0; i < y_values1.length; i++) {
-            data1.push([x_ticks[i][0] + 0.125, y_values1[i]])
-            data2.push([x_ticks[i][0] - 0.125, y_values2[i]])
-        }
-        var data = [{data: data1, color: 'red', label:'N of tries'}, {data: data2, color: 'green', yaxis: 2, label:'mean reward'}]
-
-        $.plot(canvas, data, options);
-    }
     
 
 /** 
 * Does what?
 */
 function intervalPlot(canvas, n_tries, total_reward, standard_errors) {
+    var keys = []
+    var y_values1 = []
+    var y_values2 = []
+    var sems = []
 
+    for (var key in n_tries) {
+        keys.push(key)
+        y_values1.push(n_tries[key])
+        y_values2.push(total_reward[key] / n_tries[key])
 
-        var keys = []
-        var y_values1 = []
-        var y_values2 = []
-        var sems = []
-
-        for (var key in n_tries) {
-            keys.push(key)
-            y_values1.push(n_tries[key])
-            y_values2.push(total_reward[key] / n_tries[key])
-
-			if(standard_errors != null)
-			{
-            	sems.push(standard_errors[key])
-			} else {
-				sems.push(0)
-			}
-        }
-
-
-        // Create xticks
-        var x_ticks = []
-        var x_value = 0.5
-        for (var i = 0; i < keys.length; i++) {
-            x_ticks.push([x_value, keys[i]])
-            x_value += 1
-        }
-
-        // Set up bar plot
-        var options = {
-            xaxis: {
-                min: 0,
-                max: keys.length,
-                ticks: x_ticks
-            },
-            // yaxis: {min: 0, max: 1.2},
-			// yaxis2: {min: 0}
-			yaxes: [ {min:0, max:1},{min:0, max:200} ],
-			legend: {position: "se"},
-        };
-
-        var data1 = []
-        var data2 = []
-        
-		var bar_vars = {
-			show: true,	
-			color: { opacity: 0.5 },
-			width: 0.5,		
+		if(standard_errors != null)
+		{
+        	sems.push(standard_errors[key])
+		} else {
+			sems.push(0)
 		}
-		
-        var point_vars = {
-            show: true, 
-            radius:7.5,
-			alpha: 0.9,
-            errorbars: "y",
-            yerr: {show: true, color: "red", upperCap: "-", lowerCap: '-'},
-        }
-		
-
-		
-        for (var i = 0; i < y_values1.length; i++) {
-            data1.push([x_ticks[i][0] -0.5, y_values1[i]])
-            data2.push([x_ticks[i][0], y_values2[i], sems[i]])
-        }
-        var data = [{data: data2, color: 'green', label:'Mean reward', points: point_vars},
-					{data: data1, color: 'red', label:'N. tries', color: "rgba(255,0,0,0.5)", bars: bar_vars, yaxis: 2},]
-
-        $.plot(canvas, data, options);
     }
+
+
+    // Create xticks
+    var x_ticks = []
+    var x_value = 0.5
+    for (var i = 0; i < keys.length; i++) {
+        x_ticks.push([x_value, keys[i]])
+        x_value += 1
+    }
+
+    // Set up bar plot
+    var options = {
+        xaxis: {
+            min: 0,
+            max: keys.length,
+            ticks: x_ticks
+        },
+        // yaxis: {min: 0, max: 1.2},
+		// yaxis2: {min: 0}
+		yaxes: [ {min:0, max:1},{min:0, max:200} ],
+		legend: {position: "se"},
+    };
+
+    var data1 = []
+    var data2 = []
+    
+	var bar_vars = {
+		show: true,	
+		color: { opacity: 0.5 },
+		width: 0.5,		
+	}
+	
+    var point_vars = {
+        show: true, 
+        radius:7.5,
+		alpha: 0.9,
+        errorbars: "y",
+        yerr: {show: true, color: "red", upperCap: "-", lowerCap: '-'},
+    }
+	
+    for (var i = 0; i < y_values1.length; i++) {
+        data1.push([x_ticks[i][0] -0.5, y_values1[i]])
+        data2.push([x_ticks[i][0], y_values2[i], sems[i]])
+    }
+    var data = [
+				{data: data1, color: 'red', label:'k[a]', color: "rgba(255,0,0,0.5)", bars: bar_vars, yaxis: 2},
+                {data: data2, color: 'green', label:'Q[a]', points: point_vars},
+               ]
+
+    $.plot(canvas, data, options);
+}
 
